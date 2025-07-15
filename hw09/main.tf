f9336a0706e80ae7ea65862c7cd3bbc6883dfdb5
Vladislav Pysarenko <vlad@pysarenko.com>
2025-07-15 09:51:09 +0200

    WiP

---
+++ hw09/main.tf
@@ -0,0 +1,79 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.0.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.0"
+    }
+  }
+}
+
+# Підключаємо модуль для VPC
+module "vpc" {
+  source             = "./modules/vpc"                               # Шлях до модуля VPC
+  vpc_cidr_block     = "10.0.0.0/16"                                 # CIDR блок для VPC
+  public_subnets     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"] # Публічні підмережі
+  private_subnets    = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"] # Приватні підмережі
+  availability_zones = ["us-west-2a", "us-west-2b", "us-west-2c"]    # Зони доступності
+  vpc_name           = "vpc"                                         # Ім'я VPC
+}
+
+# Підключаємо модуль ECR
+module "ecr" {
+  source           = "./modules/ecr"
+  ecr_name         = "homework-07-ecr"
+  ecr_mutable      = true
+  scan_on_push     = true
+  ecr_force_delete = true
+}
+
+module "eks" {
+  source        = "./modules/eks"
+  cluster_name  = "eks-cluster-demo"        # Назва кластера
+  subnet_ids    = module.vpc.public_subnets # ID підмереж
+  instance_type = "t3.medium"               # Тип інстансів
+  desired_size  = 1                         # Бажана кількість нодів
+  max_size      = 2                         # Максимальна кількість нодів
+  min_size      = 1                         # Мінімальна кількість нодів
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.eks.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.eks.token
+}
+
+provider "helm" {
+  kubernetes = {
+    host                   = data.aws_eks_cluster.eks.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.eks.token
+  }
+}
+
+module "jenkins" {
+  source            = "./modules/jenkins"
+  cluster_name      = module.eks.eks_cluster_name
+  oidc_provider_arn = module.eks.oidc_provider_arn
+  oidc_provider_url = module.eks.oidc_provider_url
+  kubeconfig        = data.aws_eks_cluster_auth.eks.kubeconfig
+  depends_on        = [module.eks]
+
+  providers = {
+    helm       = helm
+    kubernetes = kubernetes
+  }
+}
+
+# module "argo_cd" {
+#   source        = "./modules/argo-cd"
+#   namespace     = "argocd"
+#   chart_version = "5.46.4"
+# }
---
+++ hw09/outputs.tf
@@ -0,0 +1,51 @@
+output "eks_cluster_endpoint" {
+  value = module.eks.eks_cluster_endpoint
+}
+
+output "eks_cluster_name" {
+  value = module.eks.eks_cluster_name
+}
+
+output "eks_node_role_arn" {
+  value = module.eks.eks_node_role_arn
+}
+
+output "ecr_repository_url" {
+  value = module.ecr.ecr_repository_url
+}
+
+output "ecr_repository_arn" {
+  value = module.ecr.ecr_repository_arn
+}
+
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "public_subnets" {
+  value = module.vpc.public_subnets
+}
+
+output "private_subnets" {
+  value = module.vpc.private_subnets
+}
+
+output "internet_gateway_id" {
+  value = module.vpc.internet_gateway_id
+}
+
+output "jenkins_release" {
+  value = module.jenkins.jenkins_release_name
+}
+
+output "jenkins_namespace" {
+  value = module.jenkins.jenkins_namespace
+}
+
+output "oidc_provider_arn" {
+  value = module.eks.oidc_provider_arn
+}
+
+output "oidc_provider_url" {
+  value = module.eks.oidc_provider_url
+}
