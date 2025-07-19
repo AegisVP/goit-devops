c0fc2815361ca4ba3787bbb35c05e16934432bd1
Vlad Pysarenko <vlad@pysarenko.com>
2025-07-19 04:42:28 +0200

    add variable descriptions

--- terraform/modules/rds/variables.tf
+++ terraform/modules/rds/variables.tf
@@ -4,101 +4,123 @@ variable "name" {
 }
 
 variable "engine" {
-  type    = string
-  default = "postgres"
+  description = "Тип бази даних (наприклад, postgres, mysql)"
+  type        = string
+  default     = "postgres"
 }
 variable "engine_cluster" {
-  type    = string
-  default = "aurora-postgresql"
+  description = "Тип кластера бази даних (наприклад, aurora-postgresql)"
+  type        = string
+  default     = "aurora-postgresql"
 }
 variable "aurora_replica_count" {
-  type    = number
-  default = 1
+  description = "Кількість реплік для Aurora (для кластерів Aurora)"
+  type        = number
+  default     = 1
 }
 
 variable "aurora_instance_count" {
-  type    = number
-  default = 2 # 1 primary + 1 replica
+  description = "Кількість інстансів для Aurora (для кластерів Aurora)"
+  type        = number
+  default     = 2 # 1 primary + 1 replica
 }
 variable "engine_version" {
-  type    = string
-  default = "14.7"
+  description = "Версія бази даних (наприклад, 17.2 для PostgreSQL)"
+  type        = string
+  default     = "14.7"
 }
 
 variable "instance_class" {
-  type    = string
-  default = "db.t3.micro"
+  description = "Тип інстансу бази даних (наприклад, db.t3.medium)"
+  type        = string
+  default     = "db.t3.micro"
 }
 
 variable "allocated_storage" {
-  type    = number
-  default = 20
+  description = "Обсяг виділеного сховища в ГБ"
+  type        = number
+  default     = 20
 }
 
 variable "db_name" {
-  type = string
+  description = "Назва бази даних"
+  type        = string
 }
 
 variable "username" {
-  type = string
+  description = "Ім'я користувача бази даних"
+  type        = string
 }
 
 variable "password" {
-  type      = string
-  sensitive = true
+  description = "Пароль користувача бази даних"
+  type        = string
+  sensitive   = true
 }
 
 variable "vpc_id" {
-  type = string
+  description = "ID VPC, в якому буде розгорнуто базу даних"
+  type        = string
 }
 
 variable "subnet_private_ids" {
-  type = list(string)
+  description = "Список ID приватних підмереж для розгортання бази даних"
+  type        = list(string)
 }
 
 variable "subnet_public_ids" {
-  type = list(string)
+  description = "Список ID публічних підмереж для розгортання бази даних"
+  type        = list(string)
 }
 
 variable "publicly_accessible" {
-  type    = bool
-  default = false
+  description = "Чи буде база даних доступна з Інтернету"
+  type        = bool
+  default     = false
 }
 
 variable "multi_az" {
-  type    = bool
-  default = false
+  description = "Чи буде база даних розгорнута в кількох зонах доступності"
+  type        = bool
+  default     = false
 }
 
 variable "parameters" {
-  type    = map(string)
-  default = {}
+  description = "Налаштування параметрів бази даних"
+  type        = map(string)
+  default     = {}
 }
 
 variable "use_aurora" {
-  type    = bool
-  default = false
+  description = "Чи використовувати Aurora"
+  type        = bool
+  default     = false
 }
 
 variable "backup_retention_period" {
-  type    = string
-  default = ""
+  description = "Період зберігання резервних копій"
+  type        = string
+  default     = ""
 }
 
 variable "tags" {
-  type    = map(string)
-  default = {}
+  description = "Теги для бази даних"
+  type        = map(string)
+  default     = {}
 }
 
 variable "parameter_group_family_aurora" {
-  type    = string
-  default = "aurora-postgresql15"
+  description = "Сімейство групи параметрів для Aurora"
+  type        = string
+  default     = "aurora-postgresql15"
 }
 variable "engine_version_cluster" {
-  type    = string
-  default = "15.3"
+  description = "Версія кластера бази даних"
+  type        = string
+  default     = "15.3"
 }
 variable "parameter_group_family_rds" {
-  type    = string
-  default = "postgres15"
+  description = "Сімейство групи параметрів для RDS"
+  type        = string
+  default     = "postgres15"
 }
